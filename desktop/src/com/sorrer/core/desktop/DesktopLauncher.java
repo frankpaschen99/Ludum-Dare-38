a8411a10058b941a810bfc110458c7e23dfda65b
Sorrer <sorrer@gmail.com>
2017-04-16 00:41:09 -0400

    Libgdx

---
+++ desktop/src/com/sorrer/core/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sorrer.core.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sorrer.core.CoreGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoreGame(), config);
+	}
+}
