e1c439d0fc3ea562ca7c5d558ee9d1836c0ff640
Sorrer <sorrer@gmail.com>
2017-04-16 00:56:56 -0400

    Game now runs

--- core/src/com/sorrer/core/screens/GameScreen.java
+++ core/src/com/sorrer/core/screens/GameScreen.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 import com.sorrer.core.CoreGame;
+import com.sorrer.utils.CamUtils;
 
 public class GameScreen implements Screen{
 	
@@ -21,6 +23,25 @@ public class GameScreen implements Screen{
 	
 	
 	public GameScreen(CoreGame g){
+		this.game = g;
+		
+		this.world = new World(new Vector2(), false);
+		
+		this.b = new SpriteBatch();
+		this.sr = new ShapeRenderer();
+		
+		this.cam = new OrthographicCamera();
+		this.uicam = new OrthographicCamera();
+		uicam.viewportWidth = Gdx.graphics.getWidth();
+		uicam.viewportHeight = Gdx.graphics.getHeight();
+		uicam.position.set(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 0);
+		uicam.update();
+		
+		cam.viewportWidth = Gdx.graphics.getWidth();
+		cam.viewportHeight = Gdx.graphics.getHeight();
+		cam.position.set(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2, 0);
+		cam.update();
+		CamUtils.curCam = this.cam;
 	}
 	
 	@Override
